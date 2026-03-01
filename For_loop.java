babfdc3b26859deea63dd1fcc1d8c209bd3a5dc9
Damian Tomala <67171249+acrooxy@users.noreply.github.com>
2026-03-01 13:06:27 +0000

    Create For_loop.java

---
+++ For_loop.java
@@ -0,0 +1,12 @@
+//public class For_loop
+
+public static void main(String[] args){
+    int num = 10;
+    int total = 0;
+    for (int i = 1; i <- num; i++)
+    {
+        total = total + i ;
+
+    }
+    System.out.println("The sum of " + num + " numbers is: " + total);
+}
