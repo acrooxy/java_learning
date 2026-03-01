a5f7f8afd140028a052cd38114b00061e22df8df
Damian Tomala <67171249+acrooxy@users.noreply.github.com>
2026-03-01 13:07:01 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.sql.SQLOutput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    System.out.print("1st line ############## ");  // lol
+    System.out.println("still 1st line. Println from the nextlin \n e always");  // lol
+
+    double price;
+    price = 2.55;
+    System.out.println(price);
+
+    double times;
+    times = 5;
+
+    double result;
+
+    result = price * times;
+    System.out.println(result);
+
+    for(int i = 0; i < 10; i++) {
+        System.out.println(i);
+    }
+    for(int i = 10; i >= 1; i--) {
+        System.out.println(i);
+    }
+
+
+
+//    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//    // to see how IntelliJ IDEA suggests fixing it.
+//    IO.println(String.format("Hello and welcome!"));
+//
+//    for (int i = 1; i <= 5; i++) {
+//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//        IO.println("i = " + i);
+    {
+
+
+    }
+
+}
